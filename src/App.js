9ef5b8cf2436cedf6b6d87060126311e7d6a22c4
Fredy Velasquez <macacoloco696@gmail.com>
2023-07-08 18:33:38 -0600

    fist commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import Game from "./Game";
+import Demo from "./Demo";
+import Button from "./Button";
+import BrokenClick from "./BrokenClick";
+import Rando from "./Rando";
+import Clicker from "./Clicker";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        My App
+      </div>
+    );
+  }
+}
+
+export default App;
