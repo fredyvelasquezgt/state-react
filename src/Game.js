2951471d259165f5f084ccffbaa866b6e5f9a427
Fredy Velasquez <macacoloco696@gmail.com>
2023-07-08 18:35:03 -0600

    added Game component

---
+++ src/Game.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Game extends Componente {
+    constructor(props) {
+        super(props);
+        this.state = {
+            score: 0,
+            gameOver: false
+        }
+    }
+}
